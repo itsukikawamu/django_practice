c2273cacdfcd6dd53e5c2b2586db920c9be08641
itsukikawamu <itsuki0204@gmail.com>
2025-02-16 18:47:03 +0900

    database migrate

--- youtube/models.py
+++ youtube/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 class Channel(models.Model):
-    Channel_name = models.CharField(max_length=200)
+    channel_name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.channel_name
+    
