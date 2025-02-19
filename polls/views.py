856304992cbeb78a5d364b70e2e267f0009dfdcd
itsukikawamu <itsuki0204@gmail.com>
2025-02-20 01:44:18 +0900

    html

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    out_put = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(out_put)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("you're looking at question %s." %question_id)
