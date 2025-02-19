a349485101724acd4c81a412309b30063d250340
itsukikawamu <itsuki0204@gmail.com>
2025-02-19 12:14:41 +0900

    add views

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello,  you're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("you're looking at question %s." %question_id)
+
+def result(request, question_id):
+    response  = "you're looking at the results of quesstion %s."
+    return HttpResponse(response)
+
+def vote(requests, question_id):
+    response = "you're voting on question %s."
+    return HttpResponse(response %question_id) 
