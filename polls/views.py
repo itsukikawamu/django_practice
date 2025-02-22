df97ce4ee64c977eb97410c245abf078f36e030b
itsukikawamu <itsuki0204@gmail.com>
2025-02-22 10:42:39 +0900

    get_obj_or404

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Question
@@ -11,7 +11,8 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("you're looking at question %s." %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def result(request, question_id):
     response  = "you're looking at the results of quesstion %s."
