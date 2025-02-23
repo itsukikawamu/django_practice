7cee227b742663ce3eb4cf7f655df740bb4d53e1
itsukikawamu <itsuki0204@gmail.com>
2025-02-23 15:41:36 +0900

    add subscribers num

--- youtube/models.py
+++ youtube/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class Channel(models.Model):
     channel_name = models.CharField(max_length=200)
+    subscribers_number = models.PositiveBigIntegerField(verbose_name="登録者数")
     
     def __str__(self):
         return self.channel_name
     
+    def get_subscribers_number(self):
+        return self.subscribers_number
--- youtube/views.py
+++ youtube/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Channel
 
 def home(request):
-    context = {}
+    channel_list = Channel.objects.order_by("-subscribers_number")[:5]
+    context = {
+        "channel_list": channel_list
+    }
     return render(request, "youtube/home.html", context)
