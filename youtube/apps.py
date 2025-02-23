d6bd9d616bd11d471012683e9f4b9451c21c0a83
itsukikawamu <itsuki0204@gmail.com>
2025-02-24 00:45:42 +0900

    add post migrate

--- youtube/apps.py
+++ youtube/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
+from django.core.management import call_command
 
 class YoutubeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'youtube'
+    
+    def ready(self):
+        post_migrate.connect(migrate_youtube, sender=self)
+        
+def migrate_youtube(sender, **kwargs):
+    if kwargs.get("using") == "youtube_db":
+        return
+    call_command("migrate", database="youtube_db")
