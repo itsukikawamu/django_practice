d548c3ba307feaf9545df5f38ecb8d0b48a44945
itsukikawamu <itsuki0204@gmail.com>
2025-02-24 12:15:47 +0900

    add video page

--- youtube/urls.py
+++ youtube/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "youtube"
 urlpatterns = [
     path("", views.home, name="home"),
-    path("<slug:channel_slug>/", views.channel, name = "channel")
+    path("<slug:channel_slug>/", views.channel, name = "channel"),
+    path("<slug:channel_slug>/<slug:video_slug>/", views.video, name="video"),
 ]
--- youtube/views.py
+++ youtube/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from .models import Channel
+from .models import Channel, Video
 
 def home(request):
     channel_list = Channel.objects.order_by("-subscribers_number")[:5]
@@ -15,3 +15,12 @@ def channel(request, channel_slug):
         "channel": channel
     }
     return render(request, "youtube/channel.html", context)
+
+def video(request, channel_slug, video_slug):
+    channel = get_object_or_404(Channel, slug=channel_slug)
+    video = get_object_or_404(Video, slug=video_slug)
+    context = {
+        "channel": channel,
+        "video": video
+    }
+    return render(request, "youtube/video.html", context)
