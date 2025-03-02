e72041ee8b9ce763ee42ecbf7f4dc068b5bd5c62
itsukikawamu <itsuki0204@gmail.com>
2025-03-03 07:25:50 +0900

    make channel view class

--- youtube/views.py
+++ youtube/views.py
@@ -12,6 +12,15 @@ class HomeView(generic.TemplateView):
         context["video_list"] = Video.objects.order_by("-like_number")[:10]
         return context
     
+class ChannelView(generic.TemplateView):
+    template_name = "youtube/channel.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["channel"] = get_object_or_404(Channel, slug=kwargs["channel_slug"])
+        context["video_list"] = Video.objects.filter(channel=context["channel"]).order_by("-uploaded_at")
+        return context
+      
 class VideoView(generic.TemplateView):
     template_name = "youtube/video.html"
     
@@ -21,17 +30,5 @@ class VideoView(generic.TemplateView):
         context["video"] = get_object_or_404(Video, slug=kwargs["video_slug"], channel=context["channel"])
         return  context
 
-class ChannelView(generic.ListView):
-    model = Video
-    template_name = "youtube/channel.html"
-    context_object_name = "video_list"
-    
-    def get_queryset(self):
-        self.channel = get_object_or_404(Channel, slug=self.kwargs["channel_slug"])
-        return self.channel.video_set.order_by("-uploaded_at")
 
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context["channel"] = self.channel
-        context["channel_slug"] = self.channel.slug
-        return context
+    
