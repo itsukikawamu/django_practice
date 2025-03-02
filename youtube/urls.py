7d01e30c80ed1c5c027139a63a56f77f1241e4e3
itsukikawamu <itsuki0204@gmail.com>
2025-03-02 21:40:25 +0900

    add video view class

--- youtube/urls.py
+++ youtube/urls.py
@@ -6,5 +6,5 @@ app_name = "youtube"
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("<slug:channel_slug>/", views.ChannelView.as_view(), name = "channel"),
-    path("<slug:channel_slug>/<slug:video_slug>/", views.video, name="video"),
+    path("<slug:channel_slug>/<slug:video_slug>/", views.VideoView.as_view(), name="video"),
 ]
--- youtube/views.py
+++ youtube/views.py
@@ -2,16 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from .models import Channel, Video
-"""
-def home(request):
-    channel_list = Channel.objects.order_by("-subscribers_number")[:5]
-    video_list = Video.objects.order_by("-like_number")[:10]
-    context = {
-        "channel_list": channel_list,
-        "video_list": video_list
-    }
-    return render(request, "youtube/home.html", context)
-"""
+
 class HomeView(generic.TemplateView):
     template_name = "youtube/home.html"
     
@@ -21,13 +12,14 @@ class HomeView(generic.TemplateView):
         context["video_list"] = Video.objects.order_by("-like_number")[:10]
         return context
     
-def video(request, channel_slug, video_slug):
-    channel = get_object_or_404(Channel, slug=channel_slug)
-    video = get_object_or_404(Video, slug=video_slug, channel=channel)
-    context = {
-        "video": video
-    }
-    return render(request, "youtube/video.html", context)
+class VideoView(generic.TemplateView):
+    template_name = "youtube/video.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["channel"] = get_object_or_404(Channel, slug=kwargs["channel_slug"])
+        context["video"] = get_object_or_404(Video, slug=kwargs["video_slug"], channel=context["channel"])
+        return  context
 
 class ChannelView(generic.ListView):
     model = Video
