7dc5f104c8949aa82bd254ef46055253748f3b1c
itsukikawamu <itsuki0204@gmail.com>
2025-03-12 01:31:39 +0900

    half way

--- youtube/static/js/video.js
+++ youtube/static/js/video.js
@@ -1,5 +1,13 @@
-console.log('Hello');
+const likeForm = document.getElementById("likeForm");
+const likeButton = document.getElementById("likeButton");
+const likeCount = document.getElementById("likeCount");
 
 
+async function main(){
 
 
+    const responce = await fetch(likeForm.action, );
+    console.log(responce);
+}
+
+main();
