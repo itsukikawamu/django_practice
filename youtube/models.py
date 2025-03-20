57c26a64142c26b79e8a5b1c6cc4064d862aa3d6
itsukikawamu <itsuki0204@gmail.com>
2025-03-21 06:06:26 +0900

    make Video template looks youtube

--- youtube/models.py
+++ youtube/models.py
@@ -30,6 +30,7 @@ class Video(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     external_url = models.URLField(blank=True, null=True)
     likes = models.PositiveBigIntegerField(verbose_name="likes", default=0)
+    discription = models.CharField(max_length=1000, default="")
 
     def __str__(self):
         return self.title
