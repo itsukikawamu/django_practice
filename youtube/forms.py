25fd9385b05d2c6701593179953c50bc9b7b535e
itsukikawamu <itsuki0204@gmail.com>
2025-03-27 01:57:51 +0900

    add widget on form

--- youtube/forms.py
+++ youtube/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    keywords = forms.CharField(max_length=100, required=False)
+    keywords = forms.CharField(
+        max_length=100, 
+        required=False,
+                widget=forms.TextInput(attrs={
+            'placeholder': '検索',
+            'class': 'form-control'
+            })
+        )
