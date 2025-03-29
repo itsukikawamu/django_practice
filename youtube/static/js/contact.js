348de4568d89b7929595de6ef1ed8103ea68fc48
itsukikawamu <itsuki0204@gmail.com>
2025-03-29 22:46:42 +0900

    add setTimeout

--- youtube/static/js/contact.js
+++ youtube/static/js/contact.js
@@ -1,10 +1,18 @@
 const contactForm = document.getElementById('contactForm');
 const submitButton = document.getElementById('submitButton');
+
+function getCSRFToken() {
+    const csrfInput = document.querySelector('[name=csrfmiddlewaretoken]');
+    return csrfInput ? csrfInput.value : '';
+}
+
 function handleSubmit(event){
     event.preventDefault();
     submitButton.disabled = true;
     submitButton.textContent = '送信中...';
-    function temp(){contactForm.submit();}
-    setTimeout(temp, 1000);
+    
+    setTimeout(function(){
+        contactForm.submit();}, 2000);
 }
+
 contactForm.addEventListener('submit', handleSubmit);
